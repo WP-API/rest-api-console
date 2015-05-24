3b85d27b6da75a16a4743f65af38ac38d0449c62
Ryan McCue <me@ryanmccue.info>
2015-05-24 18:12:25 +1000

    Trim whitespace

--- rest-api-console.php
+++ rest-api-console.php
@@ -17,24 +17,23 @@ class WP_API_Console {
 		add_action( 'admin_menu', array( $this, 'admin_init' ) );
 		add_action( 'load-tools_page_rest_api_console', array( $this, 'render' ) );
 	}
-	
+
 	public function admin_init() {
 		$hook = add_management_page( 'Rest API Console', 'Rest API Console', 'manage_options', 'rest_api_console', '__return_null' );
 	}
-	
+
 	public function render() {
 		include( __DIR__ . '/templates/views/app.php' );
 		exit;
 	}
-	
-	
+
 	static function instance() {
 		static $instance = null;
-		
+
 		if ( is_null( $instance ) ) {
 			$instance = new WP_API_Console();
 		}
-		
+
 		return $instance;
 	}
 }
