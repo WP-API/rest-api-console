d419a563e141f0b0a20cf2a245e14474719b2fe8
Ryan McCue <me@ryanmccue.info>
2015-07-16 00:39:19 +0200

    Correct version

--- rest-api-console.php
+++ rest-api-console.php
@@ -3,9 +3,9 @@
  * Plugin Name: REST API Console
  * Plugin URI:  http://wordpress.org/plugins/rest-api-console/
  * Description: A console for your REST API.
- * Version:     1
- * Author:      wordpressdotorg, automattic
- * Author URI:  http://wordpress.org/
+ * Version:     2.0.1
+ * Author:      WP REST API Team
+ * Author URI:  http://wp-api.org/
  * License:     GPL v2 or later
  * Text Domain: rest-api-console
  * Domain Path: /language
